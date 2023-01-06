1dd0b7aa602f92ecaedea306e347d89928f3ef55
Clovinlee <69330330+Clovinlee@users.noreply.github.com>
2023-01-06 17:42:51 +0700

    add folow

--- app/Http/Controllers/FollowController.php
+++ app/Http/Controllers/FollowController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Follow;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
 
 class FollowController extends Controller
 {
@@ -21,6 +22,24 @@ class FollowController extends Controller
         }
         if($follow_id != null){
             $follow = $follow->where("follow_id",$follow_id);
+        }
+        $follow = $follow->values();
+        return makeJson(200,"Sukses get follows",$follow);
+    }
+
+    public function searchFollows(Request $r){
+        $id = $r->id;
+        $user_id = $r->user_id;
+        $follow_id = $r->follow_id;
+        $follow = Follow::all();
+        if($id != null){
+            $follow = $follow->where("id",$id);
+        }
+        if($user_id != null){
+            $follow = $follow->where("follow_id",$user_id)->orWhere("user_id",$user_id);
+        }
+        if($follow_id != null){
+            $follow = $follow->where("follow_id",$follow_id)->orWhere("user_id",$follow_id);
         }
         $follow = $follow->values();
         return makeJson(200,"Sukses get follows",$follow);
