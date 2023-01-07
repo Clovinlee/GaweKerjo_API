caee7999772afad5f77d659b2667941f892f1d5a
estergbriel <esther_g20@mhs.istts.ac.id>
2023-01-08 00:10:18 +0700

    benerin seeder

--- database/seeders/OfferSeeder.php
+++ database/seeders/OfferSeeder.php
@@ -31,19 +31,19 @@ class OfferSeeder extends Seeder
         DB::table("offers")->insert([
             'user_id'=>5,
             "title"=>"Customer Service",
-            "Body"=>"Perusahaan kami sedang mencari customer service untuk bekerja secara full time di kantor kami yang berada di Surabaya. Di utamakan adalah seorang fresh graduate dengan pengalaman minimal 1 tahun di bidangnya. Gaji 3.000.000 belum termasuk uang makan maupun insentif",
+            "Body"=>"Perusahaan kami sedang mencari customer service untuk bekerja secara full time di kantor kami yang berada di Surabaya. Di utamakan adalah seorang fresh graduate dengan pengalaman minimal 1 tahun di bidangnya.",
             "skills"=>"Empathy, good communication, and problem-solving"
         ]);
         DB::table("offers")->insert([
             'user_id'=>4,
             "title"=>"Digital Marketing",
-            "Body"=>"Perusahaan kami sedang mencari staff Digital Marketing untuk bekerja secara full time 8 jam per hari di perusahaan kami. Di utamakan adalah seorang dengan pengalaman minimal 3 tahun dibidangnya dan sudah mengetahui tentang google ads dan sebagainya",
+            "Body"=>"Perusahaan kami sedang mencari staff Digital Marketing untuk bekerja secara full time 8 jam per hari di perusahaan kami. Di utamakan adalah seorang dengan pengalaman minimal 3 tahun dibidangnya.",
             "skills"=>"Copywriting, Social media marketing, Search engine optimization (SEO), Project management"
         ]);
         DB::table("offers")->insert([
             'user_id'=>4,
             "title"=>"Research Assistant",
-            "Body"=>"Perusahaan kami sedang mencari research assistant untuk bekerja secara part time di perusahaan kami dengan total waktu kerja 6 jam per hari. Di utamakan adalah seorang mahasiswa dengan jurusan Informatika, Sistem Informasi di semester 3",
+            "Body"=>"Perusahaan kami sedang mencari research assistant untuk bekerja secara part time di perusahaan kami dengan total waktu kerja 6 jam per hari. Di utamakan adalah seorang mahasiswa dengan jurusan Informatika, Sistem Informasi",
             "skills"=>"Analisa data, Mengerti tentang database dan dataset"
         ]);
     }
