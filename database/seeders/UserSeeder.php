ddc00573a784e1eee0a987f723cde2681a01121e
Clovinlee <69330330+Clovinlee@users.noreply.github.com>
2023-01-09 00:52:03 +0700

    add more user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -49,7 +49,7 @@ class UserSeeder extends Seeder
         DB::table('users')->insert([
             'name' => "Markobar",
             'email' => "m@gmail.com",
-            'description' => "Aku perusahaan buatan kesang",
+            'description' => "Aku bukan perusahaan buatan kesang",
             'gender' => "L",
             'type'=>1,
             'notelp' => "9128391233",
@@ -65,6 +65,7 @@ class UserSeeder extends Seeder
             'type'=>0,
             "description"=>"Sekolah tinggi",
             "notelp"=>"08123213123",
+            "lokasi"=>"Indonesia, Surabaya",
             "password"=>"istts",
             "created_at"=>Carbon::now()
         ]);
@@ -74,9 +75,34 @@ class UserSeeder extends Seeder
             "email"=>"microsoft@com",
             'type'=>0,
             "description"=>"Windows is my windows",
+            "lokasi"=>"USA, New York",
             "notelp"=>"01234",
             "password"=>"microsoft",
             "created_at"=>Carbon::now()
+        ]);
+
+        DB::table("users")->insert([
+            "name"=>"Apple",
+            "email"=>"apple@com",
+            'type'=>0,
+            "description"=>"Apple is tech corporation",
+            "lokasi"=>"Britain, UK",
+            "notelp"=>"12111",
+            "password"=>"apple",
+            "created_at"=>Carbon::now()
+        ]);
+
+        DB::table('users')->insert([
+            'name' => "Dimas Bambang",
+            'email' => "d@gmail.com",
+            'description' => "Aku suka sepak bola , ngoding, makan, internet, tidur",
+            'gender' => "L",
+            'type'=>1,
+            'notelp' => "12315121",
+            'birthdate' => date("Y-m-d", strtotime("1980-11-15")),
+            // 'password' => Hash::make('b'),
+            'password' => "d",
+            'created_at' => Carbon::now(),
         ]);
     }
 }
